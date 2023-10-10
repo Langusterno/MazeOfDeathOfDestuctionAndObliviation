cb791a3e80e07b866406315bafd644be34565e79
CasperSmed <147515064+CasperSmed@users.noreply.github.com>
2023-10-10 15:50:08 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+tiles.setCurrentTilemap(tilemap`level1`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . 2 . 2 . . . . . . . 
+    . . . . . . 2 . 2 . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . 2 2 2 2 2 . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . . 2 . . . . . . . . 
+    . . . . . . 2 . 2 . . . . . . . 
+    . . . . . 2 . . . 2 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
